fe076465688ed0b5011bd96894ae39123fd5d1c8
Keith Ssewanonda <keith@laboremus.no>
2020-05-31 14:35:25 +0300

    chore: format code layout

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -44,30 +44,30 @@ function Game({startNewGame}) {
     return (
         <div>
             <div className="game">
-        <div className="help">
-            Pick 1 or more numbers that sum to the number of stars
-        </div>
-        <div className="body">
-            <div className="left">
-            {gamesStatus !== 'active' ? (
-                <PlayAgain onClick={startNewGame} gamesStatus={gamesStatus}/>
-            ) : (
-                <StarDisplay count={stars} />
-            )}
+                <div className="help">
+                    Pick 1 or more numbers that sum to the number of stars
+                </div>
+                <div className="body">
+                    <div className="left">
+                        {gamesStatus !== 'active' ? (
+                            <PlayAgain onClick={startNewGame} gamesStatus={gamesStatus}/>
+                        ) : (
+                            <StarDisplay count={stars} />
+                        )}
+                    </div>
+                    <div className="right">
+                        {utils.range(1, 9).map(number => 
+                            <GameNumber
+                            key={number}
+                            number={number} 
+                            status={numberStatus(number)}
+                            onclick={onNumberClick}
+                            />
+                        )}
+                    </div>
+                </div>
+                <div className="timer">Time Remaining: {secondsLeft}</div>
             </div>
-            <div className="right">
-            {utils.range(1, 9).map(number => 
-                <GameNumber
-                key={number}
-                number={number} 
-                status={numberStatus(number)}
-                onclick={onNumberClick}
-                />
-            )}
-            </div>
-        </div>
-        <div className="timer">Time Remaining: {secondsLeft}</div>
-        </div>
         </div>
     )
 }
